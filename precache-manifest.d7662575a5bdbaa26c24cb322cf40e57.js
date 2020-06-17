43846faf556bdb5fd74c0507c41fdbbbe0eb6f14
David Cho <david@peachstreet.com>
2020-06-17 00:03:43 -0400

    Updates

---
+++ precache-manifest.d7662575a5bdbaa26c24cb322cf40e57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b65a2fb6ce16ef2a9de21c33c0544a33",
+    "url": "/flashcards/index.html"
+  },
+  {
+    "revision": "0557d898935f4b5be440",
+    "url": "/flashcards/static/css/main.6da7b613.chunk.css"
+  },
+  {
+    "revision": "e91ad7f398a3242bfc00",
+    "url": "/flashcards/static/js/2.d2034dcb.chunk.js"
+  },
+  {
+    "revision": "e3cddcc14d0b349b4e6eccba623e55ae",
+    "url": "/flashcards/static/js/2.d2034dcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0557d898935f4b5be440",
+    "url": "/flashcards/static/js/main.85f0738b.chunk.js"
+  },
+  {
+    "revision": "f37a03f613c770e0327b",
+    "url": "/flashcards/static/js/runtime-main.1412b638.js"
+  }
+]);
